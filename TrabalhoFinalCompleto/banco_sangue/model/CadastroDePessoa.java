c6d1b992a8e4fdd5203b55b951497dd0926bbc83
Arthur <Arthur@Arthur-PC>
2017-05-17 23:49:30 -0300

    Atualizado BancoDeDados e add Pessoa

---
+++ TrabalhoFinalCompleto/banco_sangue/model/CadastroDePessoa.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CadastroDePessoa implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private Map<Integer, Pessoa> cadastroPessoa = new HashMap<Integer, Pessoa>();
+	public static int finalPessoa = 1;
+	//public static Pessoa pessoaDoador;
+	
+	@SuppressWarnings("unchecked")
+	public static int getTamanhoBancoPessoa() throws Exception {
+		File diretorio = new File("arqBD");
+		File arquivo = new File("arqBD\\Pessoa.dat");
+		try {
+			if (!diretorio.exists()) {
+				diretorio.mkdir();
+				return 0;
+			} else if (!arquivo.exists()) {
+				arquivo.createNewFile();
+				return 0;
+			} else {
+				Map<Integer, Pessoa> tmp = (Map<Integer, Pessoa>) BancoDeDados.buscaBDPessoa();
+				int retorno = tmp.size();
+				return retorno;
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return 0;
+		
+	}
+	
+	/*
+	 * MÉTODO QUE VERIFICA SE O BANCO DE DADOS(BD) EXISTE, SE EXISTIR ELE BUSCA O
+	 * BD, DEPOIS ELE INSERE UMA PESSOA E SOBRESCREVE O BANCO NOVAMENTE
+	 * COM A NOVA INFORMAÇÃO.
+	 */
+	
+	@SuppressWarnings("unchecked")
+	public void adicionaPessoa(Pessoa p) throws Exception{
+		BancoDeDados.verificaBDPessoa();
+		try{
+			cadastroPessoa = (Map<Integer, Pessoa>) BancoDeDados.buscaBDPessoa();
+			CadastroDePessoa.finalPessoa = cadastroPessoa.size();
+			p.setIdPessoa(finalPessoa+1);
+			cadastroPessoa.put(p.getIdPessoa(), p);
+			BancoDeDados.gravaPessoaBD(cadastroPessoa);
+		} catch (EOFException e) {
+			CadastroDePessoa.finalPessoa = cadastroPessoa.size();
+			p.setIdPessoa(finalPessoa + 1);
+			cadastroPessoa.put(p.getIdPessoa(), p);
+			BancoDeDados.gravaPessoaBD(cadastroPessoa);
+		}
+		}
+	}
+
---
+++ TrabalhoFinalCompleto/banco_sangue/model/Pessoa.java
@@ -0,0 +1,109 @@
+package model;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable, Comparable<Pessoa> {
+	
+	private static final long serialVersionUID = 1L;
+	private int idPessoa;
+	private String nome;
+	private String cpf;
+	private String telefone;
+	private String email;
+	private String endereco;
+	private String tipoSangue;
+	private double qtdDoada = 0;
+	private double totalDoacao = 0;
+	private boolean doador = false;
+	
+	public double getTotalDoacao() {
+		return totalDoacao;
+	}
+
+	public void setTotalDoacao(double totalDoacao) {
+		this.totalDoacao += totalDoacao;
+	}
+	
+	
+	public Pessoa(int idPessoa, String nome, String cpf, String telefone, String email, String endereco, String tipoSangue) {
+		setIdPessoa(idPessoa);
+		setNome(nome);
+		setCpf(cpf);
+		setTelefone(telefone);
+		setEmail(email);
+		setEndereco(endereco);
+		setTipoSangue(tipoSangue);
+	}
+	
+	public int getIdPessoa() {
+		return idPessoa;
+	}
+	public void setIdPessoa(int idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTipoSangue() {
+		return tipoSangue;
+	}
+	public void setTipoSangue(String tipoSangue) {
+		this.tipoSangue = tipoSangue;
+	}
+	
+	public double getQtdDoada() {
+		return qtdDoada;
+	}
+
+	public void setQtdDoada(double qtdDoada) {
+		this.qtdDoada = qtdDoada;
+	}
+
+	public boolean getDoador() {
+		return doador;
+	}
+
+	public void setDoador(boolean doador) {
+		this.doador = doador;
+	}
+
+	@Override
+	public int compareTo(Pessoa o) {
+		if (this.getTotalDoacao() > o.getTotalDoacao()) {
+            return -1;
+        }
+        if (this.getTotalDoacao() < o.getTotalDoacao()) {
+            return 1;
+        }
+		return 0;
+	}
+	
+
+}
